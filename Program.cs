fe3fa67eb9bb3343d831643d9405ab54cfc088e1
nkhakwan <post_khan@yahoo.com>
2020-05-07 13:42:36 -0700

    committing newly made file to test newer version of GetSet

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using Gamer.Testing;
+
+namespace Gamer
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine(GetName());
+    }
+  }
+}
