1e52007fa29fd210d900ad4c4027677ac06e40a8
nkhakwan <post_khan@yahoo.com>
2020-05-07 11:11:10 -0700

    committing completely workable dictionary file

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace CarsIOwned
 {
   public class Program
@@ -5,7 +8,7 @@ namespace CarsIOwned
     public static void Main()
     {
 
-      Dictionary<int, string> NewVenture = new Dicitionary<int, string>();
+      Dictionary<int, string> NewVenture = new Dictionary<int, string>();
 
       NewVenture.Add(1, "suzuki");
       NewVenture.Add(2, "Santro");
@@ -16,7 +19,7 @@ namespace CarsIOwned
       NewVenture.Add(7, "Nissan Quest");
       NewVenture.Add(8, "Nissan Sentra");
 
-      for (int i=0; i<NewVenture.Count; i=i+1)
+      for (int i=1; i<NewVenture.Count+1; i=i+1)
       {
         Console.WriteLine(NewVenture[i]);
       }
@@ -26,20 +29,20 @@ namespace CarsIOwned
 
       Console.WriteLine ("here is the Dictionay again with two more added");
 
-      for (int i=0; i<NewVenture.Count; i=i+1)
+      for (int i=1; i<NewVenture.Count+1; i=i+1)
       {
         Console.WriteLine(NewVenture[i]);
       }
 
 
-      for (int i=0; i<4; i=i+1)
+      for (int i=1; i<4; i=i+1)
       {
-        NewVenture.Remove(NewVenture[i]);
+        NewVenture.Remove(i);
       }
 
       Console.WriteLine("Here is the Dictionary with first four removed");
 
-      for (int i=0; i<NewVenture.Count; i=i+1)
+      for (int i=5; i<NewVenture.Count+1; i=i+1)
       {
         Console.WriteLine(NewVenture[i]);
       }
@@ -52,6 +55,17 @@ namespace CarsIOwned
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
